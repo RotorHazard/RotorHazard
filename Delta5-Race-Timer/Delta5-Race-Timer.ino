4c82d0c286342980e2feb09943f27affecf7ecde
scottgchin <chinsg@gmail.com>
2017-03-17 07:15:59 -0500

    Update Delta5-Race-Timer.ino

--- Delta5-Race-Timer/Delta5-Race-Timer.ino
+++ Delta5-Race-Timer/Delta5-Race-Timer.ino
@@ -1,5 +1,6 @@
 //Delta 5 Race Timer by Scott Chin
-//I2C functions Mike Ochtman
+//SPI driver based on fs_skyrf_58g-main.c Written by Simon Chambers
+//I2C functions by Mike Ochtman
 //Lap trigger function by Andrey Voroshkov
 
 //MIT License
