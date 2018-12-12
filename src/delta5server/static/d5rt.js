6fbadd4e2f0e0d5cb9e4cb2f6156dc235716f18d
HazardCreative <HazardCreative@users.noreply.github.com>
2018-12-12 13:46:25 -0500

    Re-implement channel blocks

--- src/delta5server/static/d5rt.js
+++ src/delta5server/static/d5rt.js
@@ -219,69 +219,71 @@ jQuery(document).ready(function($){
 /* Frequency Table */
 
 var freqTable = {
-	'--': 0,
-	R1: 5658,
-	R2: 5695,
-	R3: 5732,
-	R4: 5769,
-	R5: 5806,
-	R6: 5843,
-	R7: 5880,
-	R8: 5917,
-	F1: 5740,
-	F2: 5760,
-	F3: 5780,
-	F4: 5800,
-	F5: 5820,
-	F6: 5840,
-	F7: 5860,
-	F8: 5880,
-	E1: 5705,
-	E2: 5685,
-	E3: 5665,
-	E4: 5645,
-	E5: 5885,
-	E6: 5905,
-	E7: 5925,
-	E8: 5945,
-	B1: 5733,
-	B2: 5752,
-	B3: 5771,
-	B4: 5790,
-	B5: 5809,
-	B6: 5828,
-	B7: 5847,
-	B8: 5866,
-	A1: 5865,
-	A2: 5845,
-	A3: 5825,
-	A4: 5805,
-	A5: 5785,
-	A6: 5765,
-	A7: 5745,
-	A8: 5725,
-	L1: 5362,
-	L2: 5399,
-	L3: 5436,
-	L4: 5473,
-	L5: 5510,
-	L6: 5547,
-	L7: 5584,
-	L8: 5621,
-	U0: 5300,
-	U1: 5325,
-	U2: 5348,
-	U3: 5366,
-	U4: 5384,
-	U5: 5402,
-	U6: 5420,
-	U7: 5438,
-	U8: 5456,
-	'N/A': 'n/a',
+	frequencies: {
+		'—': 0,
+		R1: 5658,
+		R2: 5695,
+		R3: 5732,
+		R4: 5769,
+		R5: 5806,
+		R6: 5843,
+		R7: 5880,
+		R8: 5917,
+		F1: 5740,
+		F2: 5760,
+		F3: 5780,
+		F4: 5800,
+		F5: 5820,
+		F6: 5840,
+		F7: 5860,
+		F8: 5880,
+		E1: 5705,
+		E2: 5685,
+		E3: 5665,
+		E4: 5645,
+		E5: 5885,
+		E6: 5905,
+		E7: 5925,
+		E8: 5945,
+		B1: 5733,
+		B2: 5752,
+		B3: 5771,
+		B4: 5790,
+		B5: 5809,
+		B6: 5828,
+		B7: 5847,
+		B8: 5866,
+		A1: 5865,
+		A2: 5845,
+		A3: 5825,
+		A4: 5805,
+		A5: 5785,
+		A6: 5765,
+		A7: 5745,
+		A8: 5725,
+		L1: 5362,
+		L2: 5399,
+		L3: 5436,
+		L4: 5473,
+		L5: 5510,
+		L6: 5547,
+		L7: 5584,
+		L8: 5621,
+		U0: 5300,
+		U1: 5325,
+		U2: 5348,
+		U3: 5366,
+		U4: 5384,
+		U5: 5402,
+		U6: 5420,
+		U7: 5438,
+		U8: 5456,
+		'N/A': 'n/a'
+	},
 	findByFreq: function(frequency) {
-		var keyNames = Object.keys(this);
+		var keyNames = Object.keys(this.frequencies);
 		for (var i in keyNames) {
-			if (this[keyNames[i]] == frequency) {
+			if (this.frequencies[keyNames[i]] == frequency) {
 				return keyNames[i];
 			}
 		}
@@ -289,15 +291,15 @@ var freqTable = {
 	},
 	buildSelect: function() {
 		var output = '';
-		var keyNames = Object.keys(this);
+		var keyNames = Object.keys(this.frequencies);
 		for (var i in keyNames) {
-			if (typeof(this[keyNames[i]]) !== 'function') {
-				if (this[keyNames[i]] == 0) {
+			if (typeof(this.frequencies[keyNames[i]]) !== 'function') {
+				if (this.frequencies[keyNames[i]] == 0) {
 					output += '<option value="0">Disabled</option>';
-				} else if (this[keyNames[i]] == 'n/a') {
+				} else if (this.frequencies[keyNames[i]] == 'n/a') {
 					output += '<option value="n/a">N/A</option>';
 				} else {
-					output += '<option value="' + this[keyNames[i]] + '">' + keyNames[i] + ' ' + this[keyNames[i]] + '</option>';
+					output += '<option value="' + this.frequencies[keyNames[i]] + '">' + keyNames[i] + ' ' + this.frequencies[keyNames[i]] + '</option>';
 				}
 			}
 		}
