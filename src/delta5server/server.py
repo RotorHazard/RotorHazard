f61e88e40b573c06aa7eb3509e7470a735b1aa26
HazardCreative <HazardCreative@users.noreply.github.com>
2018-12-13 09:24:41 -0500

    Externalize common configuration parameters

--- src/delta5server/server.py
+++ src/delta5server/server.py
@@ -32,6 +32,22 @@ APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASEDIR, 'da
 APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 DB = SQLAlchemy(APP)
 
+# LED strip configuration:
+APP.config['LED_COUNT']      = 150      # Number of LED pixels.
+APP.config['LED_PIN']        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+APP.config['LED_FREQ_HZ']    = 800000  # LED signal frequency in hertz (usually 800khz)
+APP.config['LED_DMA']        = 10      # DMA channel to use for generating signal (try 10)
+APP.config['LED_BRIGHTNESS'] = 255     # Set to 0 for darkest and 255 for brightest
+APP.config['LED_INVERT']     = False   # True to invert the signal (when using NPN transistor level shift)
+APP.config['LED_CHANNEL']    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+APP.config['LED_STRIP']      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
+
+# other default configurations
+APP.config['HTTP_PORT'] = 5000
+
+# override defaults above with config from file
+APP.config.from_object('config')
+
 INTERFACE = get_hardware_interface()
 RACE = get_race_state() # For storing race management variables
 
@@ -46,17 +62,6 @@ import signal
 def signal_handler(signal, frame):
         colorWipe(strip, Color(0,0,0))
         sys.exit(0)
-
-# LED strip configuration:
-LED_COUNT      = 150      # Number of LED pixels.
-#LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 
 # LED one color ON/OFF
 def onoff(strip, color):
@@ -114,7 +119,7 @@ def theaterChaseRainbow(strip, wait_ms=25):
                 strip.setPixelColor(i+q, 0)
 
 # Create NeoPixel object with appropriate configuration.
-strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+strip = Adafruit_NeoPixel(APP.config['LED_COUNT'], APP.config['LED_PIN'], APP.config['LED_FREQ_HZ'], APP.config['LED_DMA'], APP.config['LED_INVERT'], APP.config['LED_BRIGHTNESS'], APP.config['LED_CHANNEL'], APP.config['LED_STRIP'])
 # Intialize the library (must be called once before other functions).
 strip.begin()
 
@@ -1351,4 +1356,4 @@ INTERFACE.set_filter_ratio_global(tune_val.f_ratio)
 print 'Server ready'
 
 if __name__ == '__main__':
-    SOCKET_IO.run(APP, host='0.0.0.0', port=80, debug=True, use_reloader=False)
+    SOCKET_IO.run(APP, host='0.0.0.0', port=APP.config['HTTP_PORT'], debug=True, use_reloader=False)
