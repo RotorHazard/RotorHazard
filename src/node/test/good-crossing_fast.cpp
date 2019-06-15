435de8b309f90a5e44b2de2ff1f39a28c7d46e86
Mark Hale <mark.hale@physics.org>
2019-06-15 23:17:16 +0100

    Updated and added more tests.

--- src/node/test/good-crossing_fast.cpp
+++ src/node/test/good-crossing_fast.cpp
@@ -69,6 +69,7 @@ unittest(fastCrossing) {
   assertEqual(70, (int)history.nadirRssi); // first downward trend
   assertEqual(timestamp(4), (int)history.nadirTime);
 
+  assertTrue(history.peakSend);
   assertEqual(130, (int)history.peakSendRssi);
   assertEqual(timestamp(3), (int)history.peakSendFirstTime);
   assertEqual(timestamp(4)-1, (int)history.peakSendLastTime);
@@ -117,6 +118,78 @@ unittest(fastCrossing) {
   assertEqual(timestamp(4)-1, (int)history.peakSendLastTime);
   assertEqual(70, (int)history.nadirSendRssi);
   assertEqual(timestamp(4), (int)history.nadirSendTime);
+}
+
+unittest(prolonged_crossing) {
+  GodmodeState* nano = GODMODE();
+  nano->reset();
+  rssiInit();
+
+  state.rxFreqSetFlag = true;
+
+  // prime the state with some background signal
+  sendSignal(nano, 50);
+  sendSignal(nano, 50);
+  assertTrue(rssiStateValid());
+
+  // enter
+  sendSignal(nano, 130);
+  int duration = 3;
+  assertLessOrEqual(1, duration);
+  for(int i=0; i<duration; i++) {
+      sendSignal(nano, 130);
+  }
+
+  assertEqual(130, (int)state.rssi);
+  assertEqual(timestamp(3+duration), (int)state.rssiTimestamp);
+  assertEqual(130, (int)state.lastRssi);
+  assertEqual(130, (int)state.nodeRssiPeak);
+  assertEqual(50, (int)state.nodeRssiNadir);
+  assertEqual(80, history.rssiChange);
+  assertTrue(state.crossing);
+
+  assertEqual(130, (int)state.passRssiPeak);
+  assertEqual(timestamp(3), (int)state.passRssiPeakFirstTime);
+  assertEqual(timestamp(3+duration), (int)state.passRssiPeakLastTime);
+  assertEqual(130, (int)history.peakRssi); // first upward trend
+  assertEqual(50, (int)history.nadirRssi); // from prolonged background signal
+  assertEqual(timestamp(3), (int)history.peakFirstTime);
+  assertEqual(timestamp(3+duration), (int)history.peakLastTime);
+
+  assertEqual(130, (int)history.peakSendRssi);
+  assertEqual(timestamp(3), (int)history.peakSendFirstTime);
+  assertEqual(timestamp(4)-1, (int)history.peakSendLastTime); // wrong
+  assertEqual(timestamp(2)-1, (int)history.nadirTime);
+  assertEqual(50, (int)history.nadirSendRssi);
+  assertEqual(timestamp(2)-1, (int)history.nadirSendTime);
+
+  // exit
+  sendSignal(nano, 70);
+  assertEqual(70, (int)state.rssi);
+  assertEqual(timestamp(4+duration), (int)state.rssiTimestamp);
+  assertEqual(130, (int)state.lastRssi);
+  assertEqual(130, (int)state.nodeRssiPeak);
+  assertEqual(50, (int)state.nodeRssiNadir);
+  assertEqual(-60, history.rssiChange);
+  assertFalse(state.crossing);
+
+  assertEqual(0, (int)state.passRssiPeak); // crossing/pass finished
+  assertEqual(timestamp(3), (int)state.passRssiPeakFirstTime);
+  assertEqual(timestamp(4+duration)-1, (int)state.passRssiPeakLastTime);
+  assertEqual(130, (int)history.peakRssi);
+  assertEqual(70, (int)history.nadirRssi); // first downward trend
+
+  assertEqual(130, (int)history.peakSendRssi);
+  assertEqual(timestamp(3), (int)history.peakSendFirstTime);
+  assertEqual(timestamp(4)-1, (int)history.peakSendLastTime); // wrong
+  assertEqual(timestamp(4+duration), (int)history.nadirTime);
+  assertEqual(50, (int)history.nadirSendRssi);
+  assertEqual(timestamp(2)-1, (int)history.nadirSendTime);
+
+  assertEqual(130, (int)lastPass.rssiPeak);
+  assertEqual(50, (int)lastPass.rssiNadir);
+  assertEqual((timestamp(3)+timestamp(4+duration)-1)/2, (int)lastPass.timestamp);
+  assertEqual(1, (int)lastPass.lap);
 }
 
 unittest_main()
