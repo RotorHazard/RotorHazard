42adab3a2df6b205a397b84ace7249478904d92a
HazardCreative <HazardCreative@users.noreply.github.com>
2019-06-19 17:01:32 -0400

    Increment dev version

--- src/server/server.py
+++ src/server/server.py
@@ -1,5 +1,5 @@
 '''RotorHazard server script'''
-RELEASE_VERSION = "2.0.0 (dev 2)" # Public release version code
+RELEASE_VERSION = "2.0.0 (dev 3)" # Public release version code
 SERVER_API = 23 # Server API version
 NODE_API_SUPPORTED = 18 # Minimum supported node version
 NODE_API_BEST = 18 # Most recent node API
