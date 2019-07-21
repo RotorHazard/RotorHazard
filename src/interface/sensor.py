93f0676fc4132e5f0e44cc434f71ceadac361c7b
Mark Hale <mark.hale@physics.org>
2019-07-21 14:53:57 +0100

    Generic sensor framework (hardware interface).

---
+++ src/interface/sensor.py
@@ -0,0 +1,30 @@
+'''Sensor class for the hardware interface.'''
+
+def Reading(units):
+    def decorator(func):
+        func.units = units
+        return func
+    return decorator
+
+class Sensor:
+    def __init__(self, name):
+        self.name = name
+
+    def getReadings(self):
+        readings = {}
+        for fname in dir(self):
+            f = getattr(self, fname)
+            if hasattr(f, 'units'):
+                readings[f.__name__] = {'value': f(), 'units': f.units}
+        return readings
+
+    def update(self):
+        pass
+
+class I2CSensor(Sensor):
+    def __init__(self, name, i2c_helper):
+        Sensor.__init__(self, name)
+        self.i2c_helper = i2c_helper
+
+    def update(self):
+        self.i2c_helper.with_i2c(self.readData)
