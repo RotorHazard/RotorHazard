b3046882bfa0e76eb1875d9a6980526b6a6ea788
E Thomas <ethomas997@etsphere.com>
2020-01-04 05:21:41 -0500

    Node code .ino stub file

---
+++ src/node/node.ino
@@ -0,0 +1,8 @@
+//This '.ino' file is a stub to support building via the original
+// Arduino IDE.  The real code is in the '.cpp' and '.h' files.
+//
+//The NODE_NUMBER definition is now in the 'rhnode.cpp' file.
+//
+//The project may be edited and built using Eclipse and the
+// "Eclipse C++ IDE for Arduino" plugin:
+// https://marketplace.eclipse.org/content/eclipse-c-ide-arduino
