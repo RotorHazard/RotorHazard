9a048e668df26b15db0588a9ed889b4e8931a747
HazardCreative <michael.niggel@gmail.com>
2020-03-17 17:25:31 -0400

    Declare reset function

--- src/node/commands.h
+++ src/node/commands.h
@@ -51,6 +51,7 @@ typedef struct Message_s Message_t;
 byte getPayloadSize(uint8_t command);
 void handleWriteCommand(Message_t *msg, bool serialFlag);
 void handleReadCommand(Message_t *msg, bool serialFlag);
+void resetPairedNode();
 
 extern uint8_t settingChangedFlags;
 
