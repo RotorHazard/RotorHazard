59c5a51ccbe6ae800ff33de5f90d18540064091d
HazardCreative <michael.niggel@gmail.com>
2020-04-06 04:21:56 -0400

    Set reset pin for Chorus wiring

--- src/node/resetNode.h
+++ src/node/resetNode.h
@@ -1,8 +1,10 @@
 // Pin to reset paired Arduino via command for ISP
-#if !ARDUVIDRX_WIRING_FLAG
-#define NODE_RESET_PIN 12
-#else
+#if ARDUVIDRX_WIRING_FLAG
 #define NODE_RESET_PIN 13
+#elif CHORUS_WIRING_FLAG
+#define NODE_RESET_PIN 13
+#else
+#define NODE_RESET_PIN 12
 #endif
 
 void initNodeResetPin();
