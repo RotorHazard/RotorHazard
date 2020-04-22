6a27fc290c0a7adda1b3db75311adf6511f1041d
ryaniftron <ryaniftron@gmail.com>
2020-04-22 00:12:50 -0600

    Add name of file in log

--- src/server/log.py
+++ src/server/log.py
@@ -4,7 +4,7 @@ import platform
 
 import gevent
 
-ROTORHAZARD_FORMAT = "<-RotorHazard-> %(message)s"
+ROTORHAZARD_FORMAT = "<-RotorHazard: %(name)s-> %(message)s"
 
 
 class GEventDeferredHandler(logging.Handler):
