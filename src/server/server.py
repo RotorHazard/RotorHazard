90a0cbe00b05bf4b749b2d3d7ed1b2c81fb0da47
E Thomas <ethomas997@etsphere.com>
2020-05-09 17:26:04 -0400

    Updated NODE_API_LEVEL to 25
    Also updated MockInterface API level to 25

--- src/server/server.py
+++ src/server/server.py
@@ -2,7 +2,7 @@
 RELEASE_VERSION = "2.2.0 (dev 5)" # Public release version code
 SERVER_API = 27 # Server API version
 NODE_API_SUPPORTED = 18 # Minimum supported node version
-NODE_API_BEST = 24 # Most recent node API
+NODE_API_BEST = 25 # Most recent node API
 JSON_API = 3 # JSON API version
 
 # This must be the first import for the time being. It is
