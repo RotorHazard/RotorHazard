7da3fa283f937d03c3b5bf047f19c9eee1dd475e
HazardCreative <michael.niggel@gmail.com>
2020-10-08 17:22:37 -0400

    Heat Unlocking UI

---
+++ src/server/static/svgasset.js
@@ -0,0 +1,3 @@
+var svg_asset = {
+	lock: '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 88 120.5" style="enable-background:new 0 0 88 120.5;" xml:space="preserve"><path d="M79.5,50.515V35c0-19.299-15.701-35-35-35s-35,15.701-35,35v15.5H8.787C3.934,50.5,0,54.434,0,59.287v52.426 c0,4.853,3.934,8.787,8.787,8.787h70.426c4.853,0,8.787-3.934,8.787-8.787V59.287C88,54.531,84.219,50.667,79.5,50.515z M25.5,35 c0-10.477,8.523-19,19-19s19,8.523,19,19v15.5h-38V35z" fill="currentColor" /></svg>'
+};
