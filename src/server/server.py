cf926c1b990033e26f73084e050a928914b4dce1
E Thomas <ethomas997@etsphere.com>
2021-01-07 03:04:26 -0500

    Updated version to 2.4.0-dev.1

--- src/server/server.py
+++ src/server/server.py
@@ -1,5 +1,5 @@
 '''RotorHazard server script'''
-RELEASE_VERSION = "2.3.0" # Public release version code
+RELEASE_VERSION = "2.4.0-dev.1" # Public release version code
 SERVER_API = 29 # Server API version
 NODE_API_SUPPORTED = 18 # Minimum supported node version
 NODE_API_BEST = 25 # Most recent node API
