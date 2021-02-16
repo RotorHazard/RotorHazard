b9e2b7ebe790b8e4db00b5c77fbcffc9739d2e33
HazardCreative <michael.niggel@gmail.com>
2021-02-15 20:33:40 -0500

    Update deprecated alias

--- src/tests/test_server.py
+++ src/tests/test_server.py
@@ -37,7 +37,7 @@ class ServerTest(unittest.TestCase):
         num_pilots = len(resp['pilots'])
         self.client.emit('add_pilot')
         resp = self.get_response('pilot_data')
-        self.assertEquals(len(resp['pilots']), num_pilots+1)
+        self.assertEqual(len(resp['pilots']), num_pilots+1)
 
     def test_alter_pilot(self):
         for i in range(1, len(server.INTERFACE.nodes)):
@@ -55,9 +55,9 @@ class ServerTest(unittest.TestCase):
                 if item['pilot_id'] == i:
                     pilot = item
                     break
-            self.assertEquals(pilot['callsign'], data['callsign'])
-            self.assertEquals(pilot['phonetic'], data['phonetic'])
-            self.assertEquals(pilot['name'], data['name'])
+            self.assertEqual(pilot['callsign'], data['callsign'])
+            self.assertEqual(pilot['phonetic'], data['phonetic'])
+            self.assertEqual(pilot['name'], data['name'])
 
     def test_add_profile(self):
         self.client.emit('load_data', {'load_types': ['node_tuning']})
@@ -65,7 +65,7 @@ class ServerTest(unittest.TestCase):
         num_profiles = len(resp['profile_ids'])
         self.client.emit('add_profile')
         resp = self.get_response('node_tuning')
-        self.assertEquals(len(resp['profile_ids']), num_profiles+1)
+        self.assertEqual(len(resp['profile_ids']), num_profiles+1)
 
     def test_alter_profile(self):
         data = {
@@ -74,8 +74,8 @@ class ServerTest(unittest.TestCase):
         }
         self.client.emit('alter_profile', data)
         resp = self.get_response('node_tuning')
-        self.assertEquals(resp['profile_name'], data['profile_name'])
-        self.assertEquals(resp['profile_description'], data['profile_description'])
+        self.assertEqual(resp['profile_name'], data['profile_name'])
+        self.assertEqual(resp['profile_description'], data['profile_description'])
 
     def test_add_race_format(self):
         self.client.emit('load_data', {'load_types': ['race_format']})
@@ -83,7 +83,7 @@ class ServerTest(unittest.TestCase):
         num_formats = len(resp['format_ids'])
         self.client.emit('add_race_format')
         resp = self.get_response('race_format')
-        self.assertEquals(len(resp['format_ids']), num_formats+1)
+        self.assertEqual(len(resp['format_ids']), num_formats+1)
 
     def test_alter_race_format(self):
         data = {
@@ -98,14 +98,14 @@ class ServerTest(unittest.TestCase):
         }
         self.client.emit('alter_race_format', data)
         resp = self.get_response('race_format')
-        self.assertEquals(resp['format_name'], data['format_name'])
-        self.assertEquals(resp['race_mode'], data['race_mode'])
-        self.assertEquals(resp['race_time_sec'], data['race_time'])
-        self.assertEquals(resp['start_delay_min'], data['start_delay_min'])
-        self.assertEquals(resp['start_delay_max'], data['start_delay_max'])
-        self.assertEquals(resp['number_laps_win'], data['number_laps_win'])
-        self.assertEquals(resp['win_condition'], data['win_condition'])
-        self.assertEquals(resp['team_racing_mode'], data['team_racing_mode'])
+        self.assertEqual(resp['format_name'], data['format_name'])
+        self.assertEqual(resp['race_mode'], data['race_mode'])
+        self.assertEqual(resp['race_time_sec'], data['race_time'])
+        self.assertEqual(resp['start_delay_min'], data['start_delay_min'])
+        self.assertEqual(resp['start_delay_max'], data['start_delay_max'])
+        self.assertEqual(resp['number_laps_win'], data['number_laps_win'])
+        self.assertEqual(resp['win_condition'], data['win_condition'])
+        self.assertEqual(resp['team_racing_mode'], data['team_racing_mode'])
 
     def test_add_race_class(self):
         self.client.emit('load_data', {'load_types': ['class_data']})
@@ -113,7 +113,7 @@ class ServerTest(unittest.TestCase):
         num_classes = len(resp['classes'])
         self.client.emit('add_race_class')
         resp = self.get_response('class_data')
-        self.assertEquals(len(resp['classes']), num_classes+1)
+        self.assertEqual(len(resp['classes']), num_classes+1)
 
     def test_alter_race_class(self):
         data = {
@@ -125,9 +125,9 @@ class ServerTest(unittest.TestCase):
         self.client.emit('alter_race_class', data)
         self.client.emit('load_data', {'load_types': ['class_data']})
         resp = self.get_response('class_data')
-        self.assertEquals(resp['classes'][0]['name'], data['class_name'])
-        self.assertEquals(resp['classes'][0]['format'], data['class_format'])
-        self.assertEquals(resp['classes'][0]['description'], data['class_description'])
+        self.assertEqual(resp['classes'][0]['name'], data['class_name'])
+        self.assertEqual(resp['classes'][0]['format'], data['class_format'])
+        self.assertEqual(resp['classes'][0]['description'], data['class_description'])
 
     def test_add_heat(self):
         self.client.emit('load_data', {'load_types': ['heat_data']})
@@ -135,7 +135,7 @@ class ServerTest(unittest.TestCase):
         num_heats = len(resp['heats'])
         self.client.emit('add_heat')
         resp = self.get_response('heat_data')
-        self.assertEquals(len(resp['heats']), num_heats+1)
+        self.assertEqual(len(resp['heats']), num_heats+1)
 
     def test_alter_heat(self):
         data = {
@@ -148,9 +148,9 @@ class ServerTest(unittest.TestCase):
         self.client.emit('alter_heat', data)
         self.client.emit('load_data', {'load_types': ['heat_data']})
         resp = self.get_response('heat_data')
-        self.assertEquals(resp['heats'][1]['pilots'][0], data['pilot'])
-        self.assertEquals(resp['heats'][1]['note'], data['note'])
-        self.assertEquals(resp['heats'][1]['class_id'], data['class'])
+        self.assertEqual(resp['heats'][1]['pilots'][0], data['pilot'])
+        self.assertEqual(resp['heats'][1]['note'], data['note'])
+        self.assertEqual(resp['heats'][1]['class_id'], data['class'])
 
 # scanner
 
@@ -236,7 +236,7 @@ class ServerTest(unittest.TestCase):
         for resp in responses:
             if resp['name'] == 'frequency_set':
                 if resp['args'][0]['node'] == 0:
-                    self.assertEquals(resp['args'][0], data)
+                    self.assertEqual(resp['args'][0], data)
                     return
 
         self.fail('No valid responses')
