9ecad73f012b3bb72c68f40a6f3dd368d4b82920
HazardCreative <michael.niggel@gmail.com>
2021-03-13 23:15:05 -0500

    Increment dev version

--- src/server/server.py
+++ src/server/server.py
@@ -1,5 +1,5 @@
 '''RotorHazard server script'''
-RELEASE_VERSION = "3.1.0-dev.1" # Public release version code
+RELEASE_VERSION = "3.1.0-dev.2" # Public release version code
 SERVER_API = 31 # Server API version
 NODE_API_SUPPORTED = 18 # Minimum supported node version
 NODE_API_BEST = 34 # Most recent node API
