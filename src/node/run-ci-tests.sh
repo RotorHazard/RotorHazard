27e16f20fffab42b2fbb497e115e55f2e1ad0563
Mark Hale <mark.hale@physics.org>
2021-04-28 17:37:32 +0100

    Fix node coverage.

---
+++ src/node/run-ci-tests.sh
@@ -0,0 +1,12 @@
+for f in test/*.cpp;
+do
+	test_name=$(basename "$f")
+	cov_dir=$(basename "$f" .cpp)
+	echo Testing $f
+	bundle exec arduino_ci.rb --skip-examples-compilation --testfile-select=$test_name
+	gcov -b -c *.cpp
+	mkdir $cov_dir
+	mv *.gcov $cov_dir
+	rm *.gcda
+	rm *.gcno;
+done
