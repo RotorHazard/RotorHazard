c36de1f9830d5b49d130cb25fac9bf7566fb5474
HazardCreative <michael.niggel@gmail.com>
2021-05-11 20:12:05 -0400

    Check correct value

--- src/server/led_handler_character.py
+++ src/server/led_handler_character.py
@@ -84,7 +84,7 @@ def printCharacter(args):
     else:
         return False
 
-    if 'color' in args and 'color':
+    if 'color' in args and args['color']:
         color = convertColor(args['color'])
     else:
         color = convertColor(ColorVal.WHITE)
@@ -124,7 +124,7 @@ def scrollText(args):
     else:
         return False
 
-    if 'color' in args and 'color':
+    if 'color' in args and args['color']:
         color = convertColor(args['color'])
     else:
         color = convertColor(ColorVal.WHITE)
