32f15e92e9e42b779656afb888577b2c2661bbd1
Mark Hale <mark.hale@physics.org>
2021-06-17 13:10:19 +0100

    Added more node tests.

--- src/node/rssirx.h
+++ src/node/rssirx.h
@@ -4,6 +4,10 @@
 #include "config.h"
 #include "rssi.h"
 #include "rx.h"
+#include "microclock.h"
+
+constexpr rssi_t MIN_RSSI_DETECT = 5; //value for detecting node as installed
+
 
 class RssiReceivers {
 public:
@@ -16,10 +20,127 @@ public:
     virtual RssiNode& getRssiNode(uint_fast8_t idx) = 0;
     virtual RxModule& getRxModule(uint_fast8_t idx) = 0;
     virtual Settings& getSettings(uint_fast8_t idx) = 0;
-    uint_fast8_t getSlotIndex(uint_fast8_t idx) const { return idx; }
+    virtual uint_fast8_t getSlotIndex(uint_fast8_t idx) const { return idx; }
     virtual uint_fast8_t getCount() const = 0;
     virtual void start(mtime_t ms, utime_t us) = 0;
     virtual bool readRssi(mtime_t ms, utime_t us) = 0;
+};
+
+
+template <typename RX> class SingleRssiReceiver final : public RssiReceivers {
+private:
+    RssiNode node;
+    RX rx;
+
+public:
+    inline RssiNode& getRssiNode(uint_fast8_t idx) { return node; }
+    inline RxModule& getRxModule(uint_fast8_t idx) { return rx; }
+    inline Settings& getSettings(uint_fast8_t idx) { return node.getSettings(); }
+    inline uint_fast8_t getCount() const { return 1; }
+    inline void start(const mtime_t ms, const utime_t us) {
+        node.start(ms, us);
+    }
+    inline bool readRssi(const mtime_t ms, const utime_t us) {
+        bool crossing = node.active && node.process(rx.readRssi(), ms);
+        node.getState().updateLoopTime(us);
+        return crossing;
+    }
+};
+
+
+template <typename RX, uint_fast8_t N> class VirtualRssiReceivers final : public RssiReceivers {
+private:
+    uint_fast8_t nodeCount = N;
+    RssiNode nodes[N];
+    RX rx;
+    int_fast8_t prevReadIndex = -1;
+    uint_fast16_t readCounter = 0;
+
+public:
+    inline RssiNode& getRssiNode(uint_fast8_t idx) { return nodes[idx]; }
+    inline RxModule& getRxModule(uint_fast8_t idx) { return rx; }
+    inline Settings& getSettings(uint_fast8_t idx) { return nodes[idx].getSettings(); }
+    inline uint_fast8_t getCount() const { return nodeCount; }
+    inline void start(const mtime_t ms, const utime_t us) {
+        // preserve order on start
+        for (uint_fast8_t i=0; i<nodeCount; i++) {
+            nodes[i].start(ms, us);
+        }
+    }
+    inline bool readRssi(const mtime_t ms, const utime_t us) {
+        if (nodeCount > 0) {
+            uint_fast8_t idx;
+            if (nodeCount > 1) {
+                // alternate the single rx between different freqs
+                idx = (readCounter++ % (READS_PER_FREQ*nodeCount))/READS_PER_FREQ;
+                if (idx != prevReadIndex) {
+                    rx.setFrequency(nodes[idx].getSettings().vtxFreq);
+                    prevReadIndex = idx;
+                }
+            } else {
+                idx = 0;
+            }
+            RssiNode& node = nodes[idx];
+            bool crossing = node.active && node.process(rx.readRssi(), ms);
+            node.getState().updateLoopTime(us);
+            return crossing;
+        } else {
+            return false;
+        }
+    }
+};
+
+
+template <typename RX, uint_fast8_t N> class PhysicalRssiReceivers final : public RssiReceivers {
+private:
+    uint_fast8_t nodeCount = N;
+    uint_fast8_t nodeToSlot[N];
+    RssiNode nodes[N];
+    RX rxs[N];
+
+public:
+    PhysicalRssiReceivers() {
+        for (int_fast8_t i=N-1; i>=0; i--) {
+            nodeToSlot[i] = i;
+        }
+    }
+
+    inline RssiNode& getRssiNode(uint_fast8_t idx) { return nodes[nodeToSlot[idx]]; }
+    inline RxModule& getRxModule(uint_fast8_t idx) { return rxs[nodeToSlot[idx]]; }
+    inline Settings& getSettings(uint_fast8_t idx) { return nodes[nodeToSlot[idx]].getSettings(); }
+    inline uint_fast8_t getSlotIndex(uint_fast8_t idx) { return nodeToSlot[idx]; }
+    inline uint_fast8_t getCount() const { return nodeCount; }
+    inline void start(const mtime_t ms, const utime_t us) {
+        uint_fast8_t sIdx=0;
+        // preserve ordering
+        for (uint_fast8_t nIdx=0; nIdx<N; nIdx++) {
+            rssi_t rssi = rxs[nIdx].readRssi();
+            if (rssi > MIN_RSSI_DETECT) {
+                nodeToSlot[sIdx++] = nIdx;
+            }
+        }
+        nodeCount = sIdx;
+
+        // preserve order on start
+        for (uint_fast8_t i=0; i<nodeCount; i++) {
+            // use latest micros() value
+            getRssiNode(i).start(ms, usclock.tickMicros());
+        }
+    }
+    inline bool readRssi(const mtime_t ms, const utime_t us) {
+        bool anyCrossing = false;
+        // preserve order on read
+        for (uint_fast8_t i=0; i<getCount(); i++) {
+            RssiNode& node = getRssiNode(i);
+            bool nodeCrossing = node.active && node.process(getRxModule(i).readRssi(), ms);
+            if (nodeCrossing) {
+                anyCrossing = true;
+            }
+            // use latest micros() value
+            node.getState().updateLoopTime(usclock.tickMicros());
+        }
+        return anyCrossing;
+    }
 };
 
 extern RssiReceivers& rssiRxs;
---
+++ src/node/test/good-rssirx.cpp
@@ -0,0 +1,48 @@
+#include <ArduinoUnitTests.h>
+#include <Godmode.h>
+#include "../rssirx.h"
+
+#define assertRssiRx(rssirx, numRxs) \
+{ \
+    assertEqual(numRxs, rssirx.getCount()); \
+    for (int i=0; i<rssirx.getCount(); i++) { \
+        rssirx.getRssiNode(i); \
+        rssirx.getRxModule(i); \
+        rssirx.getSettings(i); \
+        assertEqual(i, rssirx.getSlotIndex(i)); \
+    } \
+    rssirx.start(0,0); \
+    assertFalse(rssirx.readRssi(0, 0)); \
+}
+
+unittest(single_rx)
+{
+    SingleRssiReceiver<RX_IMPL> rssirx;
+    assertRssiRx(rssirx, 1);
+}
+
+unittest(physical_rx_0)
+{
+    PhysicalRssiReceivers<RX_IMPL,0> rssirx;
+    assertRssiRx(rssirx, 0);
+}
+
+unittest(physical_rx_2)
+{
+    PhysicalRssiReceivers<RX_IMPL,2> rssirx;
+    assertRssiRx(rssirx, 2);
+}
+
+unittest(virtual_rx_0)
+{
+    VirtualRssiReceivers<RX_IMPL,0> rssirx;
+    assertRssiRx(rssirx, 0);
+}
+
+unittest(virtual_rx_2)
+{
+    VirtualRssiReceivers<RX_IMPL,2> rssirx;
+    assertRssiRx(rssirx, 2);
+}
+
+unittest_main()
