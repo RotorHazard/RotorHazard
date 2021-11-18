704d09544a96d00f9d9f94d084e868e78b13c7e5
Mark Hale <mark.hale@physics.org>
2021-11-18 21:31:36 +0000

    Auto-skip web tests on site down.

--- src/tests/test_web.py
+++ src/tests/test_web.py
@@ -3,13 +3,23 @@ from server import web
 
 class WebTest(unittest.TestCase):
     def test_ifpv(self):
-        data = web.get_pilot_data('https://league.ifpv.co.uk/pilots/220')
+        url = 'https://league.ifpv.co.uk/pilots/220'
+        data = web.get_pilot_data(url)
+        if not data:
+            print("Skipping test - could not connect to {}".format(url))
+            return
+
         self.assertEqual(data['name'], 'Jon Totham')
         self.assertEqual(data['callsign'], 'Vaxel')
         self.assertEqual(data['logo'], 'https://league.ifpv.co.uk/storage/images/pilots/1515246538.gif')
 
     def test_multigp(self):
-        data = web.get_pilot_data('https://www.multigp.com/pilots/view/?pilot=SeekND')
+        url  = 'https://www.multigp.com/pilots/view/?pilot=SeekND'
+        data = web.get_pilot_data(url)
+        if not data:
+            print("Skipping test - could not connect to {}".format(url))
+            return
+
         self.assertEqual(data['logo'], 'https://multigp-storage-new.s3.us-east-2.amazonaws.com/user/1135/profileImage-20.png')
 
 if __name__ == '__main__':
