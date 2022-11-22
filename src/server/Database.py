1a652973b4dcd7d2525034ab685a383a0c121d38
HazardCreative <michael.niggel@gmail.com>
2022-11-22 08:09:09 -0500

    Event Scheme database setup

--- src/server/Database.py
+++ src/server/Database.py
@@ -108,6 +108,7 @@ class SavedRaceMeta(DB.Model):
     start_time_formatted = DB.Column(DB.String, nullable=False) # local human-readable time
     results = DB.Column(DB.PickleType, nullable=True)
     cacheStatus = DB.Column(DB.Integer, nullable=False)
+    scheme_race_id = DB.Column(DB.Integer, DB.ForeignKey("scheme_race.id"), nullable=True)
 
     def __repr__(self):
         return '<SavedRaceMeta %r>' % self.id
@@ -185,3 +186,45 @@ class GlobalSettings(DB.Model):
     option_name = DB.Column(DB.String(40), nullable=False)
     option_value = DB.Column(DB.String, nullable=False)
 
+class SchemeSet(DB.Model):
+    __tablename__ = 'scheme_set'
+    id = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(80), nullable=True)
+    set_format = DB.Column(DB.Integer, nullable=False)
+    rounds = DB.Column(DB.Integer, nullable=False)
+    results = DB.Column(DB.PickleType, nullable=True)
+
+    def __repr__(self):
+        return '<SchemeSet %r>' % self.id
+
+class SchemeRace(DB.Model):
+    __tablename__ = 'scheme_race'
+    id = DB.Column(DB.Integer, primary_key=True)
+    set_id = DB.Column(DB.Integer, DB.ForeignKey("scheme_set.id"), nullable=False)
+    name = DB.Column(DB.String(80), nullable=True)
+    next_race = DB.Column(DB.Integer, nullable=True)
+
+    def __repr__(self):
+        return '<SchemeRace %r>' % self.id
+
+class SchemeRaceMethodSlot(DB.Model):
+    __tablename__ = 'scheme_race_slot'
+    __table_args__ = (
+        DB.UniqueConstraint('scheme_race_id', 'node_index'),
+    )
+    id = DB.Column(DB.Integer, primary_key=True)
+    scheme_race_id = DB.Column(DB.Integer, DB.ForeignKey("scheme_race.id"), nullable=False)
+    node_index = DB.Column(DB.Integer, nullable=False)
+    method_type = DB.Column(DB.Integer, nullable=False)
+    pilot_id = DB.Column(DB.Integer, DB.ForeignKey("pilot.id"), nullable=True)
+    seed_rank = DB.Column(DB.Integer, nullable=True)
+    seed_scheme_set_id = DB.Column(DB.Integer, DB.ForeignKey("scheme_set.id"), nullable=True)
+    seed_scheme_race_id = DB.Column(DB.Integer, DB.ForeignKey("scheme_race.id"), nullable=True)
+
+    def __repr__(self):
+        return '<SchemeRaceMethodSlot %r>' % self.id
+
+class MethodType:
+    ASSIGN = 0
+    SET_RESULT = 1
+    RACE_RESULT = 2
