1ac7b2e0e466e293152d5ecd4f04ca940c1f16d1
E Thomas <ethomas997@etsphere.com>
2023-02-17 22:52:38 -0500

    Update version code to 3.3.0-dev.1

--- src/server/server.py
+++ src/server/server.py
@@ -1,5 +1,5 @@
 '''RotorHazard server script'''
-RELEASE_VERSION = "3.2.1-dev.1" # Public release version code
+RELEASE_VERSION = "3.3.0-dev.1" # Public release version code
 SERVER_API = 38 # Server API version
 NODE_API_SUPPORTED = 18 # Minimum supported node version
 NODE_API_BEST = 35 # Most recent node API
