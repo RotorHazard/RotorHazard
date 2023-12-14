ce63c1a11aa9ad31df6b7fec8878fcd328a38475
HazardCreative <HazardCreative@users.noreply.github.com>
2023-12-14 14:35:54 -0500

    Filter only databases for backup list

--- src/server/server.py
+++ src/server/server.py
@@ -1443,6 +1443,8 @@ def on_list_backups():
 
         files.sort(key=str.casefold)
 
+        files = list(filter(lambda x: x.endswith(".db"), files))
+
         emit_payload = {
             'backup_files': files
         }
