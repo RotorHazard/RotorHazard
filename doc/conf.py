8de9f41b7d59114b8522cae924da2382d3d146bd
Bryce Gruber <dev@brycegruber.com>
2024-05-10 12:57:01 -0500

    markdown test

--- doc/conf.py
+++ doc/conf.py
@@ -30,6 +30,12 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
@@ -44,5 +50,6 @@ html_theme_options = {
 }
 
 extensions = [
-              'sphinx_copybutton',
-              ]
+    'myst_parser',
+    'sphinx_copybutton',
+]
