6d8bc9b7337d84b5515c08efb176c68224a0b654
Bryce Gruber <dev@brycegruber.com>
2024-05-10 11:57:53 -0500

    importable RH

--- src/server/RHData.py
+++ src/server/RHData.py
@@ -24,7 +24,8 @@ from RHRace import RaceStatus, WinCondition, StagingTones
 from Database import ProgramMethod, HeatAdvanceType, HeatStatus
 
 from FlaskAppObj import APP
-APP.app_context().push()
+if APP:
+    APP.app_context().push()
 
 class RHData():
     _OptionsCache = {} # Local Python cache for global settings
