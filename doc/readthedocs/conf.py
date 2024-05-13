06c210970dcaccc375bcaf55e00b1c017b2ceb0f
Bryce Gruber <dev@brycegruber.com>
2024-05-12 21:22:24 -0500

    update dir

--- doc/readthedocs/conf.py
+++ doc/readthedocs/conf.py
@@ -9,8 +9,8 @@
 import os
 import sys
 
-docdir = os.path.split(os.path.split(__file__)[0])[0]
-topdir = os.path.split(os.path.split(docdir)[0])[0]
+topdir = os.path.split(os.path.split(os.path.split(__file__)[0])[0])[0]
+
 sys.path.insert(0, os.path.join(topdir, "src/server"))
 sys.path.insert(0, os.path.join(topdir, "src/server/util"))
 sys.path.insert(0, os.path.join(topdir, "src/interface"))
