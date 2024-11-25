de776f0e14100862e401b6d13f18bdd7083264a8
HazardCreative <michael.niggel@gmail.com>
2024-11-24 19:01:56 -0500

    Update version to v4.2.0-beta.3

--- src/server/server.py
+++ src/server/server.py
@@ -1,5 +1,5 @@
 '''RotorHazard server script'''
-RELEASE_VERSION = "4.2.0-dev.4" # Public release version code
+RELEASE_VERSION = "4.2.0-beta.3" # Public release version code
 SERVER_API = 45 # Server API version
 NODE_API_SUPPORTED = 18 # Minimum supported node version
 NODE_API_BEST = 35 # Most recent node API
