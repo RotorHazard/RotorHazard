10a5e7086d4b9c493fcada11b5a6c5a1a8fb6f33
E Thomas <ethomas997@etsphere.com>
2025-03-08 03:13:34 -0500

    Enhance save-logs fn with more files

--- src/server/log.py
+++ src/server/log.py
@@ -452,7 +452,7 @@ def delete_old_log_files(num_keep_val, lfname, lfext, err_str):
     return num_del, err_str
 
 
-def create_log_files_zip(logger, config_file, db_file, outData=None, boot_config_file="/boot/firmware/config.txt", \
+def create_log_files_zip(logger, config_file, db_file, program_dir_str, outData=None, boot_config_file="/boot/firmware/config.txt", \
                          alt_boot_config_file="/boot/config.txt"):
     zip_file_obj = None
     try:
@@ -468,16 +468,25 @@ def create_log_files_zip(logger, config_file, db_file, outData=None, boot_config
                 if root == LOG_DIR_NAME:  # don't include sub-directories
                     for fname in files:
                         zip_file_obj.write(os.path.join(root, fname))
+            service_file = '/lib/systemd/system/rotorhazard.service'
             try:
-                # also include configuration, database, and OS boot-config files
+                # include configuration, database, .bashrc, OS boot-config files, etc
                 if config_file and os.path.isfile(config_file):
                     zip_file_obj.write(config_file)
                 if db_file and os.path.isfile(db_file):
                     zip_file_obj.write(db_file)
+                bashrc_file = os.path.expanduser('~/.bashrc')
+                if os.path.isfile(bashrc_file):
+                    zip_file_obj.write(bashrc_file, 'bashrc.txt')
                 if boot_config_file and os.path.isfile(boot_config_file):
                     zip_file_obj.write(boot_config_file, boot_config_file[1:].replace('/','_'))
                 elif alt_boot_config_file and os.path.isfile(alt_boot_config_file):
                     zip_file_obj.write(alt_boot_config_file, alt_boot_config_file[1:].replace('/','_'))
+                if os.path.isfile(service_file):
+                    zip_file_obj.write(service_file, 'rotorhazard.service.txt')
+                server_file = os.path.join(program_dir_str,  'server.py')
+                if os.path.isfile(server_file):
+                    zip_file_obj.write(server_file, 'server.py')
             except Exception:
                 logger.exception("Error adding files to log-files .zip file")
             try:
@@ -490,7 +499,7 @@ def create_log_files_zip(logger, config_file, db_file, outData=None, boot_config
             except Exception:
                 logger.exception("Error adding audio settings info to .zip file")
             try:
-                # also include current list of Python libraries
+                # include current list of Python libraries
                 whichPipStr = subprocess.check_output(['which', 'pip']).decode("utf-8").rstrip()
                 if whichPipStr:  # include execution path to 'pip'
                     whichPipStr = "$ " + whichPipStr + " list" + os.linesep
@@ -505,6 +514,16 @@ def create_log_files_zip(logger, config_file, db_file, outData=None, boot_config
                 zip_file_obj.writestr('pip_libs_list.txt', fetchedStr)
             except Exception:
                 logger.exception("Error adding pip-list libraries info to .zip file")
+            try:
+                # fetch output of "systemctl status rotorhazard.service" command
+                if os.path.isfile(service_file):
+                    fetchedStr =  subprocess.run(['systemctl', 'status', 'rotorhazard.service'], \
+                                  stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=False).stdout.\
+                                  decode("utf-8").rstrip()
+                    if fetchedStr:
+                        zip_file_obj.writestr('service_status.txt', fetchedStr)
+            except Exception:
+                logger.exception("Error adding service-status info to .zip file")
             zip_file_obj.close()
             return zip_path_name
     except Exception:
