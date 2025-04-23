74ae3ed67bc033d3fd640a261b0d7851c65e31d5
HazardCreative <michael.niggel@gmail.com>
2025-04-23 18:18:05 -0400

    Interface mapper improvements
    Clarify var names
    Store references in node

--- src/server/interface_mapper.py
+++ src/server/interface_mapper.py
@@ -25,21 +25,22 @@ class InterfaceMeta:
 
 class InterfaceMapper:
     def __init__(self):
-        self._interfaces = []
-        self._nodemap = []
+        self._interface_map = []
+        self._node_map = []
 
     def add_interface(self, interface, if_type:InterfaceType, args=None):
-        interface.pass_record_callback = self.pass_record_callback
-        interface.new_enter_or_exit_at_callback = self.new_enter_or_exit_at_callback
-        interface.node_crossing_callback = self.node_crossing_callback
-        self._interfaces.append(InterfaceMeta(interface, if_type))
+        self._interface_map.append(InterfaceMeta(interface, if_type))
+
+        for node in interface.nodes:  # store node provider and type
+            node.provider = interface
+            node.provider_type = if_type
 
         if if_type == InterfaceType.MOCK:
             for node in interface.nodes:  # put mock nodes at latest API level
                 node.api_level = args['api_level']
 
         for idx, node in enumerate(interface.nodes):
-            self._nodemap.append(
+            self._node_map.append(
                 NodeMap(
                     interface=interface,
                     type=if_type,
@@ -48,10 +49,24 @@ class InterfaceMapper:
                 )
             )
 
+    def add_callbacks(self):
+        for ifmeta in self._interface_map:
+            ifmeta.interface.pass_record_callback = self.pass_record_callback
+            ifmeta.interface.new_enter_or_exit_at_callback = self.new_enter_or_exit_at_callback
+            ifmeta.interface.node_crossing_callback = self.node_crossing_callback
+
+    def reindex_nodes(self):
+        for idx, node_map in enumerate(self._node_map):
+            node_map.object.index = idx
+
+    @property
+    def mapped_interfaces(self):
+        return self._interface_map
+
     @property
     def nodes(self):
         nodelist = []
-        for node in self._nodemap:
+        for node in self._node_map:
             nodelist.append(node.object)
         return nodelist
 
@@ -65,19 +80,19 @@ class InterfaceMapper:
         pass
 
     def start(self):
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             iface.interface.start()
 
     def stop(self):
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             iface.interface.stop()
 
     def update_loop(self):
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             iface.interface.update_loop()
 
     def update(self):
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             iface.interface.update()
 
     #
@@ -85,66 +100,66 @@ class InterfaceMapper:
     #
 
     def set_frequency(self, node_index, frequency):
-        mapped_node = self._nodemap[node_index]
+        mapped_node = self._node_map[node_index]
         local_index = mapped_node.index
         return mapped_node.interface.set_frequency(local_index, frequency)
 
     def transmit_enter_at_level(self, node, level):
-        for mapped_node in self._nodemap:
+        for mapped_node in self._node_map:
             if node == mapped_node:
                 return mapped_node.interface.transmit_enter_at_level(node, level)
 
     def set_enter_at_level(self, node_index, level):
-        mapped_node = self._nodemap[node_index]
+        mapped_node = self._node_map[node_index]
         local_index = mapped_node.index
         return mapped_node.interface.set_enter_at_level(local_index, level)
 
     def transmit_exit_at_level(self, node, level):
-        for mapped_node in self._nodemap:
+        for mapped_node in self._node_map:
             if node == mapped_node:
                 return mapped_node.interface.transmit_exit_at_level(node, level)
 
     def set_exit_at_level(self, node_index, level):
-        mapped_node = self._nodemap[node_index]
+        mapped_node = self._node_map[node_index]
         local_index = mapped_node.index
         return mapped_node.interface.set_exit_at_level(local_index, level)
 
     def force_end_crossing(self, node_index):
-        mapped_node = self._nodemap[node_index]
+        mapped_node = self._node_map[node_index]
         local_index = mapped_node.index
         return mapped_node.interface.force_end_crossing(local_index)
 
     def jump_to_bootloader(self):
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             iface.interface.jump_to_bootloader()
 
     def send_status_message(self, msgTypeVal, msgDataVal):
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             if iface.type == InterfaceType.RH:
                 return iface.interface.send_status_message()
 
     def send_shutdown_button_state(self, stateVal):
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             if iface.type == InterfaceType.RH:
                 return iface.interface.send_shutdown_button_state()
 
     def send_shutdown_started_message(self):
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             if iface.type == InterfaceType.RH:
                 return iface.interface.send_shutdown_started_message()
 
     def send_server_idle_message(self):
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             if iface.type == InterfaceType.RH:
                 return iface.interface.send_server_idle_message()
 
     def get_fwupd_serial_name(self):
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             if iface.type == InterfaceType.RH:
                 return iface.interface.get_fwupd_serial_name()
 
     def close_fwupd_serial_port(self):
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             iface.interface.close_fwupd_serial_port()
 
     def get_info_node_obj(self):
@@ -152,35 +167,35 @@ class InterfaceMapper:
 
     def get_intf_total_error_count(self):
         count = 0
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             count += iface.interface.get_intf_total_error_count()
         return count
 
     def set_intf_error_report_percent_limit(self, percentVal):
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             iface.interface.set_intf_error_report_percent_limit(percentVal)
 
     def get_intf_error_report_str(self, **kwargs):
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             if iface.type == InterfaceType.RH:
                 return iface.interface.get_intf_error_report_str(**kwargs)
 
     # From Base
 
     def get_lap_source_str(self, source_idx):
-        return self._interfaces[0].get_lap_source_str(source_idx)
+        return self._interface_map[0].interface.get_lap_source_str(source_idx)
 
     #
     # External functions for setting data
     #
 
     def intf_simulate_lap(self, node_index, ms_val):
-        mapped_node = self._nodemap[node_index]
+        mapped_node = self._node_map[node_index]
         local_index = mapped_node.index
         return mapped_node.interface.intf_simulate_lap(local_index, ms_val)
 
     def set_race_status(self, race_status):
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             iface.interface.set_race_status(race_status)
 
     def set_calibration_threshold_global(self, threshold):
@@ -196,12 +211,12 @@ class InterfaceMapper:
         return threshold  # dummy function; no longer supported
 
     def start_capture_enter_at_level(self, node_index):
-        mapped_node = self._nodemap[node_index]
+        mapped_node = self._node_map[node_index]
         local_index = mapped_node.index
         return mapped_node.interface.start_capture_enter_at_level(local_index)
 
     def start_capture_exit_at_level(self, node_index):
-        mapped_node = self._nodemap[node_index]
+        mapped_node = self._node_map[node_index]
         local_index = mapped_node.index
         return mapped_node.interface.start_capture_exit_at_level(local_index)
 
@@ -211,7 +226,7 @@ class InterfaceMapper:
 
     def get_heartbeat_json(self):
         json = {}
-        for iface in self._interfaces:
+        for iface in self._interface_map:
             data = iface.interface.get_heartbeat_json()
             for key, val in data.items():
                 if key in json:
